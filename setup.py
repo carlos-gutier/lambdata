e3c2f2c9f16aa8719f428a4cf1b618bc07e205dd
Carlos Gutierrez <chgutier@gmail.com>
2019-02-13 18:32:36 -0600

    Edited version to 0.0.7 in setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata-carlosgutier",
-    version="0.0.6",
+    version="0.0.7",
     author="carlos-gutier",
     description="A collection of Data Science helper functions",
     long_description=LONG_DESCRIPTION,
